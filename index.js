e15068658c14178f5c556537cc7055d520ca8a39
kittymom123 <114093873+kittymom123@users.noreply.github.com>
2023-02-24 03:16:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+addEventListener(
+  "fetch",event => {
+     let url=new URL(event.request.url);
+     url.hostname="bilidm.vip";  //你需要反代的域名
+     let request=new Request(url,event.request);
+     event. respondWith(
+       fetch(request)
+     )
+  }
+)
